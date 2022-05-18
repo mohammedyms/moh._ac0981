fcbcc6268e7230a7dbd6baf0e4629c32cfe5c908
mohammedyms <105734480+mohammedyms@users.noreply.github.com>
2022-05-18 11:55:45 +0300

    Add files via upload

---
+++ WindowsFormsApp4/WindowsFormsApp4/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace WindowsFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnlogin = new System.Windows.Forms.Button();
+            this.btncan = new System.Windows.Forms.Button();
+            this.txtusername = new System.Windows.Forms.TextBox();
+            this.txtpassword = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnlogin
+            // 
+            this.btnlogin.Location = new System.Drawing.Point(105, 282);
+            this.btnlogin.Name = "btnlogin";
+            this.btnlogin.Size = new System.Drawing.Size(119, 43);
+            this.btnlogin.TabIndex = 3;
+            this.btnlogin.Text = "Login";
+            this.btnlogin.UseVisualStyleBackColor = true;
+            this.btnlogin.Click += new System.EventHandler(this.btnlogin_Click);
+            // 
+            // btncan
+            // 
+            this.btncan.Location = new System.Drawing.Point(242, 282);
+            this.btncan.Name = "btncan";
+            this.btncan.Size = new System.Drawing.Size(125, 43);
+            this.btncan.TabIndex = 4;
+            this.btncan.Text = "Cancel";
+            this.btncan.UseVisualStyleBackColor = true;
+            // 
+            // txtusername
+            // 
+            this.txtusername.Location = new System.Drawing.Point(154, 138);
+            this.txtusername.Name = "txtusername";
+            this.txtusername.Size = new System.Drawing.Size(164, 22);
+            this.txtusername.TabIndex = 1;
+            // 
+            // txtpassword
+            // 
+            this.txtpassword.Location = new System.Drawing.Point(154, 213);
+            this.txtpassword.Name = "txtpassword";
+            this.txtpassword.Size = new System.Drawing.Size(164, 22);
+            this.txtpassword.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(154, 115);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "User name ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(154, 193);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 17);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Password";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(472, 440);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtpassword);
+            this.Controls.Add(this.txtusername);
+            this.Controls.Add(this.btncan);
+            this.Controls.Add(this.btnlogin);
+            this.Name = "Form1";
+            this.Text = "mh_aab9810";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnlogin;
+        private System.Windows.Forms.Button btncan;
+        private System.Windows.Forms.TextBox txtusername;
+        private System.Windows.Forms.TextBox txtpassword;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WindowsFormsApp4/WindowsFormsApp4/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        userman usr = new userman();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void btnlogin_Click(object sender, EventArgs e)
+        {
+            string username = "mohammad", password = "1234";
+            if (txtusername.Text == username && txtpassword.Text == password)
+            {
+                usr.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("un correct password !!!");
+            }
+        }
+    }
+}
